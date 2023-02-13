21c817be46aa0f1f3a6ca152204450d6841959a2
JaegerDaFBoi <mcallister2696@gmail.com>
2023-02-13 07:47:16 -0600

    routes configured

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +26,17 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::controller(EmployeeController::class)->group(function () {
+    Route::prefix('employee')->group(function () {
+        Route::name('employee.')->group(function () {
+            Route::get('/index', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::post('/store', 'store')->name('store');
+            Route::get('/edit', 'edit')->name('edit');
+            Route::patch('/update', 'update')->name('update');
+            Route::delete('/delete', 'destroy')->name('delete');
+        });
+    });
 });
